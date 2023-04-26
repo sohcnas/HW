d7d3cf178293166527195a8498e238db329ae2ca
Alexandr Taravkov <sohcnas@yandex.ru>
2023-04-26 06:14:21 +0300

    программа возврата квадрата числа

---
+++ task0/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на входи принимает число и выдает его квадрат
+
+// Например:
+// 4 -> 16
+// 3 -> 9
+// 7 -> 49
+
+Console.WriteLine("ВВедите число:");
+int number = int.Parse(Console.ReadLine());
+number = number * number;
+Console.WriteLine(number);
