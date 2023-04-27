a5bd423aec03d03fdcf0dede635921f60be1be28
Alexandr Taravkov <sohcnas@yandex.ru>
2023-04-27 06:55:56 +0300

    внес коррективы в вывод, результата.

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Написать программу, которая вводит два числа и проверяет являетс ли первое квадратом второго
+
+// 25 -> 5 да 
+// 5 -> 10 нет
+
+Console.WriteLine("Введите первое число:"); // подсказка для пользователя
+int number = int.Parse(Console.ReadLine()); // получаем число
+Console.WriteLine("Введите второе число число:");
+int sqr = int.Parse(Console.ReadLine());
+if (number * number == sqr)
+    Console.WriteLine("Квадрат нашего сисла равен:" + number * number); // склейка сообщения + результат
+else 
+    Console.WriteLine("NO");
+
