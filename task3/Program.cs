e81ad97b521978f246b7eef64b4b9c1fabc9a384
Alexandr Taravkov <sohcnas@yandex.ru>
2023-04-27 19:46:37 +0300

    решение задачи с числом N и выводом последовательности -N до N

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу, которая на вход принимает одно число N, а на выход выход выводит 
+// последовательность от -N до N
+
+Console.WriteLine("Введите чило N");
+int N = int.Parse(Console.ReadLine());
+int i = -N;
+while (i <= N)
+{
+    Console.Write(i + " ");
+    i++;
+}
