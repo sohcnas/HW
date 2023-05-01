041f1fca6091a4e6e09cba4e38f0f7e729ac29a3
Alexandr Taravkov <sohcnas@yandex.ru>
2023-05-01 06:47:41 +0300

    добавлено решение задачи 10 домашняя работа 2

---
+++ task4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число и выводит последнюю его цифру
+
+Console.WriteLine("Введите трехзначное число");
+int Number = int.Parse(Console.ReadLine());
+if ( Number > 99 && Number < 1000)
+{
+    Console.WriteLine("Последняя цифра числа: " + Number%10);
+}
