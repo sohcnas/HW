5f49be4f5b4f61f7d4e1fd327ddf54fd24ff02a2
Alexandr Taravkov <sohcnas@yandex.ru>
2023-05-11 14:26:59 +0300

    задача вывода значения координат по номеру четверти

---
+++ s3task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+main();
+
+void main ()
+{
+    int QuaterNumber = Prompt ("Введите номер четверти: ");
+    PrintCoordinate (QuaterNumber);
+}
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+void PrintCoordinate (int Number)
+{
+    if (Number == 1)
+    {
+       Console.WriteLine("X > 0 и Y > 0"); 
+    }
+        if (Number == 2)
+    {
+       Console.WriteLine("X < 0 и Y > 0"); 
+    }
+        if (Number == 3)
+    {
+       Console.WriteLine("X < 0 и Y < 0"); 
+    }
+        if (Number == 4)
+    {
+       Console.WriteLine("X > 0 и Y < 0"); 
+    }
+}
