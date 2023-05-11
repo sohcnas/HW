af08f2a6a9d78c74bb4dfa53b84af6835f60aad0
Alexandr Taravkov <sohcnas@yandex.ru>
2023-05-11 20:18:31 +0300

    задача 23 выполнена

--- C#_HW3/task23/Program.cs
+++ C#_HW3/task23/Program.cs
@@ -4,3 +4,26 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Main ();
+
+void Main ()
+{
+    int Number = Prompt ("Введите число: ");
+    CalculationCube(Number);
+}
+
+int Prompt (string Value)
+{
+    Console.Write(Value);
+    int Number = int.Parse(Console.ReadLine());
+    return Number;
+}
+
+void CalculationCube (int Number)
+{
+    for (int i = 1 ; i <= Number; i++)
+    {
+        Console.WriteLine("Куб " + i + " = " + Math.Pow(i, 3));
+    }
+}
