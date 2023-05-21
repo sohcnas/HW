bd77be75f8b0676a634db07f42f7a7bbd5c10837
Alexandr Taravkov <sohcnas@yandex.ru>
2023-05-21 12:42:43 +0300

    Семинар 4 задача 26 подсчет количества цифр в числе

---
+++ task26/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Main ();
+
+void Main ()
+{
+    NumberCount(Prompt());
+}
+
+int Prompt ()
+{
+    Console.WriteLine("Введите произвольное целое число: ");
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void NumberCount (int number)
+{
+    int count = 0;
+    for(int i = 1; i < number; i = i * 10)
+    {
+        count++;
+    }
+    Console.WriteLine($"В числе {number} цифр {count}");
+}
