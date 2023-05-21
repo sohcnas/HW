703997bd45fa504cc1e48f952382c8d60c0b05aa
Alexandr Taravkov <sohcnas@yandex.ru>
2023-05-21 13:07:20 +0300

    Семинар 4 задача 28 произведение чисел от 1 до числа N

---
+++ task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Main ();
+
+void Main ()
+{
+    int limit = Prompt(); // введеное пользователем значение
+    int result = 1;  // накапливаемая сумма значения
+    for(int i = 1; i <= limit; i++)
+    {
+        result = result * i;
+    }
+    if (limit == 0 ) result = 0;
+    Console.WriteLine($"Результат равен: {result}");
+}
+
+int Prompt ()
+{
+    Console.WriteLine("Введите число: ");
+    int limit = int.Parse(Console.ReadLine());
+    return limit;
+}
