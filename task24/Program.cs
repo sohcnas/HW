9b3d7ae34d97e7869f7096d7efe565417c16db6b
Alexandr Taravkov <sohcnas@yandex.ru>
2023-05-21 12:24:49 +0300

    Семинар 4, задача 24. Вывод суммы чисел до А

---
+++ task24/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Main ();
+
+void Main ()
+{
+    int limit = Prompt(); // введеное пользователем значение
+    int result = 0;  // накапливаемая сумма значения
+    for(int i = 1; i <= limit; i++)
+    {
+        result = result + i;
+    }
+    Console.WriteLine($"Результат равен: {result}");
+}
+
+int Prompt ()
+{
+    Console.WriteLine("Введите число: ");
+    int limit = int.Parse(Console.ReadLine());
+    return limit;
+}
