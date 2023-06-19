b5e96944e7eeedcd5ee6b8db8657f01992a64fb6
Alexandr Taravkov <sohcnas@yandex.ru>
2023-06-20 00:26:56 +0300

    task68

--- C#_HW9/task68/Program.cs
+++ C#_HW9/task68/Program.cs
@@ -3,3 +3,29 @@
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+int m = Prompt("Введите число m: ");
+int n = Prompt("Введите число n: ");
+Console.WriteLine(Akkerman(m, n));
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 & n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else 
+    {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+}
