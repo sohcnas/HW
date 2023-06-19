a846e9028f310f5b2b09ae1c3ec7c180ea4d107f
Alexandr Taravkov <sohcnas@yandex.ru>
2023-06-19 19:53:17 +0300

    HW9

---
+++ C#_HW9/task64/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в 
+// промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+int n = Prompt("Введите число n: ");
+int m = Prompt("Введите число m: ");
+PrintNumber(m, n);
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+void PrintNumber(int m, int n)
+{
+    if (m > n)
+    {
+        Console.Write($" {n}");
+        PrintNumber(m, n + 1);
+
+    }
+    else if (m < n)
+    {
+        Console.Write($" {m}");
+        PrintNumber(m + 1, n);
+    }
+    else
+    {
+        Console.Write($" {m}");
+        Console.WriteLine();
+    }
+}
---
+++ C#_HW9/task66/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int n = Prompt("Введите число n: ");
+int m = Prompt("Введите число m: ");
+PrintSumNumber(m, n, 0);
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+void PrintSumNumber(int m, int n, int sum)
+{
+    if (m > n)
+    {
+        PrintSumNumber(m, n + 1, sum+=n);
+
+    }
+    else if (m < n)
+    {
+        PrintSumNumber(m + 1, n, sum+=m);
+    }
+    else
+    {
+        sum+=m;
+        Console.Write($"{sum}");
+        Console.WriteLine();
+    }
+}
